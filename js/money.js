eea67828ef5a6a89123907e9daa002c9fdbf4dcc
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-17 16:44:36 +0600

    add income express calculation

--- js/money.js
+++ js/money.js
@@ -1,1 +1,34 @@
-console.log('hro')
+function calculateBtn() {
+
+    const incomeInput = getInput('income-input');
+
+
+    const foodInput = getInput('food-input');
+    const rentInput = getInput('rent-input');
+    const clothesInput = getInput('clothes-input');
+
+
+    const totalCost = foodInput + rentInput + clothesInput;
+
+    const displayTotalCost = displayOutput('total-expenses');
+    displayTotalCost.innerText = totalCost;
+
+    const displayRest = incomeInput - totalCost;
+
+    const balense = displayOutput('balance');
+    balense.innerText = displayRest;
+
+}
+
+function displayOutput(outputId) {
+    const output = document.getElementById(outputId);
+    return output;
+}
+
+function getInput(inputId) {
+    const inputs = document.getElementById(inputId).value;
+
+    const transFlotValue = parseFloat(inputs);
+
+    return transFlotValue;
+}
